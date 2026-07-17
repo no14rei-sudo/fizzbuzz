e73273858730e33507d95ae4c9c7b34a64a3b92d
no14rei-sudo <no14.rei@gmail.com>
2026-07-17 16:09:02 +0900

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.job("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.job("Fizz");
+    } else if (i % 5 === 0) {
+        console.job("Buzz");
+    } else {
+        console.job(i);
+    }
+}
