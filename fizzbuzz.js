0f8b751e0a53f39655200fadfe4436286f3da726
no14rei-sudo <no14.rei@gmail.com>
2026-07-17 16:15:06 +0900

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,10 @@
 for (let i = 1; i <= 20; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.job("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.job("Fizz");
     } else if (i % 5 === 0) {
         console.job("Buzz");
+    } else if (i % 3 === 0) {
+        console.job("Fizz");
     } else {
         console.job(i);
     }
